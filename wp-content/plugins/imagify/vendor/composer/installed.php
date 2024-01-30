a11c3ba573660f5bc7207a2a6ceb5b7050937b84
binhchay1 <binhchay1@gmail.com>
2024-01-30 14:04:12 +0700

    change imagify

---
+++ wp-content/plugins/imagify/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-media/imagify-plugin',
+        'pretty_version' => 'v2.1.3.1',
+        'version' => '2.1.3.1',
+        'reference' => '82a34afe494abd2ba229285f871ab23239428a20',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'reference' => 'c29dc4b93137acb82734f672c37e029dfbd95b35',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'dangoodman/composer-for-wordpress' => array(
+            'pretty_version' => '2.0.2',
+            'version' => '2.0.2.0',
+            'reference' => 'cc5b3d0a1122d87d60f378071159bac0dbd93daa',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../dangoodman/composer-for-wordpress',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-media/imagify-plugin' => array(
+            'pretty_version' => 'v2.1.3.1',
+            'version' => '2.1.3.1',
+            'reference' => '82a34afe494abd2ba229285f871ab23239428a20',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
