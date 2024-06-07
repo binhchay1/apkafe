96501962a2a42e504336cfedba82657e87a70cb3
binhchay1 <binhchay1@gmail.com>
2024-06-07 10:55:26 +0700

    change plugin

--- wp-content/plugins/easy-accordion-free/admin/GutenbergBlock/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => 'e7676e789e165931b092');
--- wp-content/plugins/easy-accordion-free/includes/updates/update-2.0.6.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Update version.
- *
- * @package easy-accordion-free
- */
-
-update_option( 'easy_accordion_free_version', '2.0.6' );
-update_option( 'easy_accordion_free_db_version', '2.0.6' );
